a92f7af330655348ad55379b736c448478c9cae7
Johnson Li <johnsonli1993@163.com>
2016-01-17 19:13:12 +0800

    add some jar

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 jdk8=''
+build=''
 
 while [[ $# > 0 ]]
 do
@@ -9,14 +10,20 @@ do
         -8|--jdk8)
         jdk8='true'
         ;;
+        -b|--build)
+        build='true'
     esac
     shift
 done
 
-./gradlew build
+if [ -n "$build" ];
+then
+    ./gradlew build
+fi
+
 if [ -z "$jdk8" ];
 then
-    java -Xbootclasspath/p:lib/alpn-boot-7.1.3.v20150130.jar -cp ./lib/log4j-core-2.5.jar:./out/artifacts/poseidon_jar/poseidon.jar http2.client.ClientMain
+    java -Xbootclasspath/p:lib/alpn-boot-7.1.3.v20150130.jar -cp ./lib/log4j-core-2.5.jar:./lib/poseidon.jar http2.client.ClientMain
 else
-    java -Xbootclasspath/p:lib/alpn-boot-8.1.4.v20150727.jar -cp ./lib/log4j-core-2.5.jar:./out/artifacts/poseidon_jar/poseidon.jar http2.client.ClientMain
+    java -Xbootclasspath/p:lib/alpn-boot-8.1.4.v20150727.jar -cp ./lib/log4j-core-2.5.jar:./lib/poseidon.jar http2.client.ClientMain
 fi
