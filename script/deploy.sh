edf1783f43e72228fcca221bbeadf35412bcd978
Johnson Li <johnsonli1993@163.com>
2016-04-03 17:16:57 +0800

    fix run script

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,9 @@
 if [ ! -d '../planet_lab/lib' ]; then
     mkdir ../planet_lab/lib
 fi
+if [ -d '../planet_lab/jre1.8.0_51' ]; then
+    rm -rf ../planet_lab/jre1.8.0_51
+fi
 cp ../lib/alpn-boot-8.1.4.v20150727.jar ../planet_lab/lib
 cp ../build/libs/poseidon-all-1.0-SNAPSHOT.jar ../planet_lab
 cp run.sh ../planet_lab
--- script/run.sh
+++ script/run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-java -Xbootclasspath/p:./lib/alpn-boot-8.1.4.v20150727.jar -cp -jar poseidon-all-1.0-SNAPSHOT.jar http2.client.ClientMain
+JAVA_HOME='jre1.8.0_51'
+JAVA_CMD="$JAVA_HOME/bin/java"
+exec "$JAVA_CMD" -Xbootclasspath/p:./lib/alpn-boot-8.1.4.v20150727.jar -jar poseidon-all-1.0-SNAPSHOT.jar http2.client.ClientMain
