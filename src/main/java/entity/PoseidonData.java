c5d48f617690ff8a62c19a3d83af18829b696baf
Johnson Li <johnsonli1993@163.com>
2016-09-25 16:36:28 +0800

    add note to poseidon data

--- src/main/java/entity/PoseidonData.java
+++ src/main/java/entity/PoseidonData.java
@@ -14,11 +14,13 @@ public class PoseidonData {
   ConfigData config;
   @SerializedName("http2_unsupported")
   boolean http2Unsupported;
+  String note = null;
 
   public PoseidonData(ExperimentData http1Data, ExperimentData http2Data, ConfigData config, boolean http2Unsupported) {
     this.http1Data = http1Data;
     this.http2Data = http2Data;
     this.config = config;
     this.http2Unsupported = http2Unsupported;
+    this.note = "non_parallel";
   }
 }
